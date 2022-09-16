0a6a58bf877eb3e152051d84b2772c9e6a471c9e
NeoYC777 <neochangphoto@gmail.com>
2022-09-17 00:47:57 +0800

    Add about, portfolio, and contact page link

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ app.engine('handlebars', exphbs({defaultLayout:'main'}))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/about', (req, res)=>{
+  res.render('about')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.listen(port, () => {
+  console.log(`Express is runnung on http://localhost:${port}`)
+})
